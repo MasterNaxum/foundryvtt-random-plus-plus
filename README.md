41705ea20e39ef8e5131b566109a1f765aa1242d
mbtorrero <m.blanco@cognifit.com>
2022-05-02 15:25:49 +0200

    Version 1.0.1

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# foundryvtt-random-plus-plus
-Slightly alters the default FoundryVTT's RNG algorythm.
+
+# Random++
+Slightly changes pseudo-random number generation in mysterious ways.
+
+This FoundryVTT Module gives clients slightly different options to alter their Random Number Generation. It does so by modifying the `int()` method of the MersenneTwister's class definition (used to generate random numbers). The method's intent is to return a 32 bit number (between 0 and 2^32-1), which is then used by the implementation in all other rng related queries of the application (mainly, rolling dice).
+
+The options available are as follow.
+
+## Mersenne Twister (Original)
+Makes no changes to the RNG generation. This is the default value. Uses the Mersenne Twister implementation of FoundryVTT.
+
+## Mersenne - Reseed after every roll
+This option will take the intended result calculated by the Mersenne Twister, and before returning it, it will re-seed the twister. 
+
+The new seed is the XOR of the last 32 bits of `Date.now()` (time of the roll) and the result that is actually returned.
+
+**Why use this?**
+Reseeding after every roll will re-calculate the state of the Mersenne Twister. Since we are setting a random seed that originates from the current time of the roll and the already arbitrary last roll, there should not be much of a difference from the original method.
+
+## Mersenne - Reseed before every roll
+This option will re-seed the Mersenne Twister before rolling.
+
+The new seed is the XOR of the last 32 bits of `Date.now()` (time of the roll) and the last result returned by the Mersenne Twister.
+
+**Why use this?**
+Reseeding before every roll using a random seed that originates from the current time of the roll and the already arbitrary last roll should make the new roll less "predictable", since it will be dependant on the exact time the roll took place.
+
+## Quantum RNG - Bulk (qrng.anu.edu.au)
+This option will skip using the Mersenne Twister for the most part, leaving the RNG generation to making requests to the website https://qrng.anu.edu.au/. 
+
+It will save a number of values that originate from quantum readings at the **Centre for Quantum Computing and Communication Technology** of **The Australian National University** at **Canberra**.
+
+Through the settings of this module, set how many numbers to request at a time to keep in memory. When the time comes to roll, the next number on the list is used, and then removed from the list. If the list ever shrunks more than the specified setting, a request will be made to fill the list with a number equal to the specified setting. If there aren't any numbers left on the list, we default to calling the default Mersenne Twister method and we make sure to send a request for more numbers, which may or may not come in time for the next call.
+
+**Why use this?**
+Reading quantum fluctuations is theoretically more random than any pseudo-random generator algorythm out there. It is "true randomness". We should ask the ANU for data in bulks in order to not saturate their servers by making a request for every single individual roll that we want.
+
+Detaching the RNG generation from the client is also arguably better.
+
+*NOTE: This will expose the N next results to the client, but in the future, this module will implement an option to ask for every single individual roll as part of an asynchronous request.*
--- scripts/random-plus-plus.js
+++ scripts/random-plus-plus.js
@@ -1,22 +1,53 @@
-class RandomPPModule {
-  
+class RandomPPModule 
+{
+	
 	static ID = 'random-plus-plus';
 	
-	static FLAGS = {
-		
-	}
-	
-	static TEMPLATES = {
-		
+	static MESSAGES = {
+		INIT_START: { key: "init-start", ifnot: "%c > Random++ %c Initializing..."},
+		INIT_FAILED_LW: { key: "init-failed-missing-lw", ifnot: "%c > Random++ %c Initialization failed: libWrapper module was not found on init hook."},
+		INIT_SUCCESS: { key: "init-success", ifnot: "%c > Random++ %c Initialized!"},
+		INIT_FAILED_LW_GM: { key: "init-failed-missing-lw-gm", ifnot: "The Random++ module requires the libWrapper module. Please install and activate it."},
+		READY_SUCCESS: { key: "ready-success", ifnot: "%c > Random++ %c Ready!"},
+		FIRE_INIT_HOOK: { key: "fire-init-hook", ifnot: "%c > Random++ %c Firing init hook." },
+		FIRE_READY_HOOK: { key: "fire-ready-hook", ifnot: "%c > Random++ %c Firing ready hook." }
 	}
 	
 	static SETTINGS = {
 		METHOD: 'method',
+		BULKSIZE: 'bulk-size'
+	}
+
+	static localize(key = "", ifnot = "")
+	{
+		const res = game.i18n.localize(key);
+		return res === key ? ifnot : res;
 	}
 	
 	static initialize() 
 	{
-		//console.log("RandomPPModule::initialize");
+		RandomPPModule.debug = false;
+		
+		RandomPPModule.lastResult = twist.int(); //Before it has been replaced by lib-wrapper.
+		RandomPPModule.u32RandomArray = [];
+		RandomPPModule.bulkSize = 8;
+		
+		var choices = {
+			"original": `RANDOM-PLUS-PLUS.settings.${this.SETTINGS.METHOD}.OptionOriginal`,
+			"reseed": `RANDOM-PLUS-PLUS.settings.${this.SETTINGS.METHOD}.OptionReseedAfter`,
+			"reseed-before": `RANDOM-PLUS-PLUS.settings.${this.SETTINGS.METHOD}.OptionReseedBefore`,
+			"remote-anu-bulk": `RANDOM-PLUS-PLUS.settings.${this.SETTINGS.METHOD}.OptionRemoteANUBulk`
+		};
+		
+		//Only enable remote-anu-single as an option if this is version 0.9.0 or higher.
+		var versionRegexp = /[0]\.[0-8].[0-9]/;
+		var versionString = game.version ? game.version : game.data.version;
+		if((RandomPPModule.debug && !versionRegexp.test(versionString)))
+		{
+			choices["remote-anu-single"] = `RANDOM-PLUS-PLUS.settings.${this.SETTINGS.METHOD}.OptionRemoteANUSingle`;
+		}
+		
+		//Register setting: Method
 		game.settings.register(
 			this.ID, 
 			this.SETTINGS.METHOD, 
@@ -26,58 +57,190 @@ class RandomPPModule {
 				type: String,
 				scope: 'client',
 				config: true,
-				choices: {
-					"original": `RANDOM-PLUS-PLUS.settings.${this.SETTINGS.METHOD}.OptionOriginal`,
-					"reseed": `RANDOM-PLUS-PLUS.settings.${this.SETTINGS.METHOD}.OptionReseed`
-			    },
+				choices: choices,
 				hint: `RANDOM-PLUS-PLUS.settings.${this.SETTINGS.METHOD}.Hint`,
-				onChange: value => {  
-					this.useNewMethod = value === "reseed";
+				onChange: value => {
+					RandomPPModule.SetRNGMethod(value);
 				}
 			}
 		);
 		
-		this.useNewMethod = game.settings.get(RandomPPModule.ID, RandomPPModule.SETTINGS.METHOD) === "reseed";
+		//Register setting: Buffer/Bulk size
+		game.settings.register(
+			this.ID, 
+			this.SETTINGS.BULKSIZE, 
+			{
+				name: `RANDOM-PLUS-PLUS.settings.${this.SETTINGS.BULKSIZE}.Name`,
+				default: 32,
+				type: Number,
+				scope: 'client',
+				config: true,
+				range: {
+					min: 8,
+					max: 1024,
+					step: 1
+				},
+				hint: `RANDOM-PLUS-PLUS.settings.${this.SETTINGS.BULKSIZE}.Hint`,
+				onChange: value => {
+					RandomPPModule.SetRNGBulk(value);
+				}
+			}
+		);
+		
+		RandomPPModule.SetRNGMethod(game.settings.get(RandomPPModule.ID, RandomPPModule.SETTINGS.METHOD));
+		RandomPPModule.bulkSize = game.settings.get(RandomPPModule.ID, RandomPPModule.SETTINGS.BULKSIZE);
 	}
 	
-	static SetRNGMethod(useNewMethod = false) {
-		this.useNewMethod = useNewMethod;
-	}
-	
-	static NewSeed() {
-		if(twist)
-		{
-			twist.seed(twist.int() ^ (Date.now() & 4294967295) );
+	static SetRNGMethod(method = "original") {
+		switch(method){
+			default:
+			case "original":
+				RandomPPModule.method = method;
+			break;
+			case "reseed":
+				RandomPPModule.method = method;
+			break;
+			case "reseed-before":
+				RandomPPModule.method = method;
+			break;
+			case "remote-anu-bulk":
+				RandomPPModule.method = method;
+				RandomPPModule.bulkSize = game.settings.get(RandomPPModule.ID, RandomPPModule.SETTINGS.BULKSIZE);
+				RandomPPModule.RequestRemoteRNGResults();
+			break;
+			case "remote-anu-single":
+				if(RandomPPModule.debug)
+				{
+					RandomPPModule.method = method;
+					RandomPPModule.bulkSize = 1;
+				}
+				else
+				{
+					RandomPPModule.method = "original";
+				}
+			break;
 		}
 	}
 	
-	static CSS() { 
+	static SetRNGBulk(value = 32)
+	{
+		RandomPPModule.bulkSize = value;
+	}
+	
+	static get CSS() { 
 		return "color:#FFFFFF; background-image: linear-gradient(#0000DD, #000088); font-weight: bold; border: 0.1vh solid white; border-radius: 0.5vh; padding: 0.5vh; box-shadow: 0px 0px 1vh black;";
 	}
+	
+	static RequestSingleRNG(fallbackResult = 0){
+		if(!!RandomPPModule.RemoteRequestInProgress) return;
+		var remoteRequest = new XMLHttpRequest();
+		remoteRequest.open("GET", "https://qrng.anu.edu.au/API/jsonI.php?length=1&type=hex16&size=4", false);
+		remoteRequest.send(null);
+		let response = JSON.parse(remoteRequest.response);
+		return (remoteRequest.status === 200 && response.success) ? response.data[0] : fallbackResult;
+	}
+	
+	static RequestRemoteRNGResults(){
+		if(!!RandomPPModule.RemoteRequestInProgress) return;
+		var remoteRequest = new XMLHttpRequest();
+		remoteRequest.open("GET", `https://qrng.anu.edu.au/API/jsonI.php?length=${RandomPPModule.bulkSize}&type=hex16&size=4`, true);
+		remoteRequest.onload = function (e) {
+			if (remoteRequest.readyState === 4) {
+				if (remoteRequest.status === 200) {
+					let response = JSON.parse(remoteRequest.response);
+					if(!response.success)
+					{
+						RemoteRequestInProgress = false;
+						return;
+					}
+					let toAdd = [];
+					response.data.forEach( x => { toAdd.push(parseInt(x, 16) >>> 0); });
+					RandomPPModule.u32RandomArray.push(...toAdd);
+				} else {
+					console.error(remoteRequest.statusText);
+				}
+			RandomPPModule.RemoteRequestInProgress = false;
+			}
+		};
+		remoteRequest.onerror = function (e) {
+			console.error(remoteRequest.statusText);
+			RandomPPModule.RemoteRequestInProgress = false;
+		};
+		remoteRequest.send(null);
+	}
+	
 }
 
 Hooks.once('init', () => {
-    console.log("%c > Random++ %c Init hook called.", RandomPPModule.CSS(), "");
+	console.log(RandomPPModule.localize(RandomPPModule.MESSAGES.INIT_START.key, RandomPPModule.MESSAGES.INIT_START.ifnot), RandomPPModule.CSS, "");
+	if(!game.modules.get('lib-wrapper')?.active)
+	{
+		console.log(RandomPPModule.localize(RandomPPModule.MESSAGES.INIT_FAILED_LW.key, RandomPPModule.MESSAGES.INIT_FAILED_LW.ifnot), RandomPPModule.CSS, "");
+		return;
+	}
 	RandomPPModule.initialize();
+	console.log(RandomPPModule.localize(RandomPPModule.MESSAGES.INIT_SUCCESS.key, RandomPPModule.MESSAGES.INIT_SUCCESS.ifnot), RandomPPModule.CSS, "");
+	Hooks.callAll('random-plus-plus.init');
+	console.log(RandomPPModule.localize(RandomPPModule.MESSAGES.FIRE_INIT_HOOK.key, RandomPPModule.MESSAGES.FIRE_INIT_HOOK.ifnot), RandomPPModule.CSS, "");
 });
 
 Hooks.once('ready', () => {
-    console.log("%c > Random++ %c Ready hook called.", RandomPPModule.CSS(), "");
-	
-	if(!game.modules.get('lib-wrapper')?.active && game.user.isGM)
+
+	if(!game.modules.get('lib-wrapper')?.active)
 	{
-        ui.notifications.error("The module [ Random++ ] requires the 'libWrapper' module. Please install and activate it.");
+		if(game.user.isGM)
+		{
+			ui.notifications.error(RandomPPModule.localize(RandomPPModule.MESSAGES.INIT_FAILED_LW_GM.key, RandomPPModule.MESSAGES.INIT_FAILED_LW_GM.ifnot));
+		}
 		return;
 	}
-	
+
 	game.modules.get('random-plus-plus').api = {
 		SetRNGMethod: RandomPPModule.SetRNGMethod,
-		NewSeed: RandomPPModule.NewSeed
+		RequestRemoteRNGResults: RandomPPModule.RequestRemoteRNGResults
 	};
 	
 	libWrapper.register('random-plus-plus', 'MersenneTwister.prototype.int', function (wrapped, ...args) {
-		let result = wrapped(...args);
-		if(!RandomPPModule.useNewMethod) this.seed(result ^ (Date.now() & 4294967295))
-		return result;
-	}, 'WRAPPER');
+		switch(RandomPPModule.method)
+		{
+			default:
+			case "original":
+				RandomPPModule.lastResult = wrapped(...args);
+				return RandomPPModule.lastResult;
+			break;
+			case "reseed":
+				RandomPPModule.lastResult = wrapped(...args);
+				this.seed(RandomPPModule.lastResult ^ (Date.now() & 0xFFFFFFFF));
+				return RandomPPModule.lastResult;
+			break;
+			case "reseed-before":
+				this.seed(RandomPPModule.lastResult ^ (Date.now() & 0xFFFFFFFF));
+				RandomPPModule.lastResult = wrapped(...args);
+				return RandomPPModule.lastResult;
+			break;
+			case "remote-anu-single":
+				RandomPPModule.lastResult = RandomPPModule.RequestSingleRNG(wrapped(...args));
+				return RandomPPModule.lastResult;
+			break;
+			case "remote-anu-bulk":
+				if(RandomPPModule.u32RandomArray.length == 0)
+				{
+					RandomPPModule.RequestRemoteRNGResults(); //Request more
+					RandomPPModule.lastResult = wrapped(...args); //Fallback to original MersenneTwister since there's no more.
+					return RandomPPModule.lastResult;
+				}
+				RandomPPModule.lastResult = RandomPPModule.u32RandomArray.shift() >>> 0;
+				if(RandomPPModule.u32RandomArray.length < RandomPPModule.bulkSize)
+				{
+					RandomPPModule.RequestRemoteRNGResults();
+				}
+				return RandomPPModule.lastResult;
+			break;
+		}
+		
+	}, libWrapper.MIXED);
+	
+	console.log(RandomPPModule.localize(RandomPPModule.MESSAGES.READY_SUCCESS.key, RandomPPModule.MESSAGES.READY_SUCCESS.ifnot), RandomPPModule.CSS, "");
+	console.log(RandomPPModule.localize(RandomPPModule.MESSAGES.FIRE_READY_HOOK.key, RandomPPModule.MESSAGES.FIRE_READY_HOOK.ifnot), RandomPPModule.CSS, "");
+	Hooks.callAll('random-plus-plus.ready');
 });
